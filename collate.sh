7bb5906d7d3a3f2f89f8e532c9f3f90f849bd06d
Joseph Redmon <pjreddie@gmail.com>
2018-10-07 12:19:15 -0700

    add collation

---
+++ collate.sh
@@ -0,0 +1,20 @@
+rm -rf submit/
+mkdir -p submit
+
+prepare () {
+    if [[ $(git diff $1 | wc -c) -eq 0 ]]; then 
+        echo "WARNING: $1 is unchanged."
+    fi
+    cp $1 submit/
+}
+
+echo "Creating tarball..."
+prepare src/matrix.c
+prepare src/activations.c
+prepare src/connected_layer.c
+prepare mnist_pytorch.ipynb
+
+tar cvzf submit.tar.gz submit
+rm -rf submit/
+echo "Done. Please upload submit.tar.gz to Canvas."
+
