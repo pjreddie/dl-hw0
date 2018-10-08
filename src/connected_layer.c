a3318341be40b61bf44951f8e40e4cf9155539c0
Althea Poteet <althea@D-10-18-235-10.dhcp4.washington.edu>
2018-10-08 11:08:59 -0700

    fixed some dimensions problems, now have a malloc error (tf?)

--- src/connected_layer.c
+++ src/connected_layer.c
@@ -53,11 +53,23 @@ for that layer.
 // returns: the same layer, modified after running
 matrix forward_connected_layer(layer l, matrix in)
 {
+    printf("forward hi!");
     // TODO: 3.1 - run the network forward
     matrix out = make_matrix(in.rows, l.w.cols); // Going to want to change this!
+    // printf("in: %d\n%d\n\n", in.rows, in.cols);
+    // printf("weights: %d\n%d\n\n", l.w.rows, l.w.cols);
     out = matmul(in, l.w);
-    printf("%d %d", in.row, in.col);
-    printf("%d %d", l.b.row, l.b.col);
+    // printf("out: %d\n%d\n\n", out.rows, out.cols);
+    // printf("bias: %d\n%d\n\n", l.b.rows, l.b.cols);
+    matrix new_b = make_matrix(out.rows, out.cols);
+    int i, j;
+    for(i = 0; i < out.rows; i++) {
+        for(j = 0; j < out.cols; j++) {
+            new_b.data[i * out.cols + j] = l.b.data[j];
+        }
+    }
+    l.b = new_b;
+    // printf("bias (updated): %d\n%d\n\n", l.b.rows, l.b.cols);
     axpy_matrix(1.0, l.b, out);
     activate_matrix(out, l.activation);
 
@@ -76,30 +88,37 @@ matrix forward_connected_layer(layer l, matrix in)
 // matrix delta: 
 void backward_connected_layer(layer l, matrix prev_delta)
 {
-    printf("hi!");
+    printf("backward hi!");
     matrix in    = l.in[0];
     matrix out   = l.out[0];
     matrix delta = l.delta[0];
-
+    printf("mo");
     // TODO: 3.2
     // delta is the error made by this layer, dL/dout
     // First modify in place to be dL/d(in*w+b) using the gradient of activation
     gradient_matrix(out, l.activation, delta);
-
+    printf("blow");
     // Calculate the updates for the bias terms using backward_bias
     // The current bias deltas are stored in l.db
     backward_bias(delta, l.db);
-
+    printf("crow");
     // Then calculate dL/dw. Use axpy_matrix to add this dL/dw into any previously stored
     // updates for our weights, which are stored in l.dw
-    matrix weight_loss = matmul(delta, transpose_matrix(in));
+    // printf("\ndelta: %d\n%d\n\n", delta.rows, delta.cols);
+    // printf("\nin: %d\n%d\n\n", in.rows, in.cols);
+    matrix weight_loss = matmul(transpose_matrix(in), delta);
+    // printf("\nweight loss: %d\n%d\n\n", weight_loss.rows, weight_loss.cols);
+    // printf("scarecrow");
+    // printf("\ndelta weights: %d\n%d\n\n", l.dw.rows, l.dw.cols);
     axpy_matrix(1.0, weight_loss ,l.dw);
-
+    printf("dlow");
     if(prev_delta.data){
         // Finally, if there is a previous layer to calculate for,
         // calculate dL/d(in). Again, using axpy_matrix, add this into the current
         // value we have for the previous layers delta, prev_delta.
-        matrix in_loss = matmul(delta, transpose_matrix(l.w));
+        printf("\nyo!\n");
+        matrix in_loss = matmul(transpose_matrix(l.w), delta);
+        printf("\nno!\n");
         axpy_matrix(1.0, in_loss, prev_delta);
     }
 }
@@ -107,6 +126,7 @@ void backward_connected_layer(layer l, matrix prev_delta)
 // Update 
 void update_connected_layer(layer l, float rate, float momentum, float decay)
 {   
+    printf("update hi!");
     axpy_matrix(-1 * decay, l.w, l.dw);
     axpy_matrix(rate, l.dw, l.w);
     scal_matrix(momentum, l.dw);
